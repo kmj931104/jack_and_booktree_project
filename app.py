237e2a37cadcd7ddabbcb5c5e25cd764c5afa4a2
minjoo <kim9min3joo@gmail.com>
2021-01-12 02:49:56 +0900

    app.py 4
    db 올리기 성공...드디어.....

--- app.py
+++ app.py
@@ -2,22 +2,21 @@ from flask import Flask, render_template, jsonify, request
 import requests
 from pymongo import MongoClient
 
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
 app = Flask(__name__)
 
-#
-# @app.route('/')
-# def home_jack_and_beantree():
-#     return render_template('index.html')
-#
-#
-# @app.route('/booksadd')
-# def book_tree_add():
-#     return render_template('booktree_add.html')
 
 @app.route('/')
-def temp_home():
+def home_jack_and_booktree():
+    return render_template('index.html')
+
+
+@app.route('/booksadd')
+def book_tree_add():
     return render_template('booktree_add.html')
+
 
 @app.route('/booksadd', methods=['POST'])
 def adding_book_tree():
@@ -30,14 +29,25 @@ def adding_book_tree():
     date = request.form['date']
     rate = request.form['rate']
 
-    print(title, author, page, isbn, sentence, comment, date, rate)
+    doc = {
+        'title': title,
+        'author': author,
+        'page': page,
+        'isbn': isbn,
+        'sentence': sentence,
+        'comment': comment,
+        'date': date,
+        'rate': rate
+    }
+
+    db.booktree.insert_one(doc)
+    return jsonify({'result': 'success', 'msg': '잭의 책나무가 한뼘 자라났습니다!'})
 
 
-#
-# @app.route('/booktree')
-# def book_tree_now():
-#     return render_template('beanTree.html')
-#
+@app.route('/booktree')
+def book_tree_now():
+    return render_template('beanTree.html')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
