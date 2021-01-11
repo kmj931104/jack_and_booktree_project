3f608f7c8bebfd8bdc2a3d52dd2fa41d59478dfc
minjoo <kim9min3joo@gmail.com>
2021-01-11 22:43:18 +0900

    app.py 1차
    route 설정하기

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home_jack_and_beantree():
+    return render_template('index.html')
+
+@app.route('/books')
+def book_tree_current():
+    return render_template('booktree_page.html')
+
+@app.route('/books/add')
+def book_tree_adding():
+    return render_template('booktree_add.html')
+
+@app.route('/booktree')
+def book_tree_now():
+    return render_template('beanTree.html')
 
 
 if __name__ == '__main__':
