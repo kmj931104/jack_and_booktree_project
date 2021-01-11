1944cafde88f05f5f754aff470abd377d00a0712
minjoo <kim9min3joo@gmail.com>
2021-01-12 02:15:10 +0900

    app.py _ db에 올리기
    왜 안돼ㅠㅠㅠㅠㅠㅠㅠ뭐가 문제야ㅠㅠㅠ

--- app.py
+++ app.py
@@ -5,35 +5,39 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
+#
+# @app.route('/')
+# def home_jack_and_beantree():
+#     return render_template('index.html')
+#
+#
+# @app.route('/booksadd')
+# def book_tree_add():
+#     return render_template('booktree_add.html')
 
 @app.route('/')
-def home_jack_and_beantree():
-    return render_template('index.html')
-
-# @app.route('/books')
-# def book_tree_current():
-#     return render_template('booktree_page.html')
-
-@app.route('/books', methods=['GET'])
-def book_tree_adding():
+def temp_home():
     return render_template('booktree_add.html')
 
-def book_added():
-    title = request.form['title'];
-    author = request.form['author'];
-    page = request.form['title'];
-    isbn = request.form['isbn'];
-    sentence = request.form['sentence'];
-    comment = request.form['comment'];
-    date = request.form['date'];
-    rate =request.form['rate'];
+@app.route('/booksadd', methods=['POST'])
+def adding_book_tree():
+    title = request.form['title']
+    author = request.form['author']
+    page = request.form['page']
+    isbn = request.form['isbn']
+    sentence = request.form['sentence']
+    comment = request.form['comment']
+    date = request.form['date']
+    rate = request.form['rate']
 
-    print(title, author, page, isbn, sentence, comment, date, rate);
+    print(title, author, page, isbn, sentence, comment, date, rate)
 
-@app.route('/booktree')
-def book_tree_now():
-    return render_template('beanTree.html')
 
+#
+# @app.route('/booktree')
+# def book_tree_now():
+#     return render_template('beanTree.html')
+#
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
