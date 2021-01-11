3c390cf11b969ead79dcea3537f764ffb5fb8302
minjoo <kim9min3joo@gmail.com>
2021-01-11 23:38:53 +0900

    app.py 2차
    데이터 가져오는걸 해봤는데(ajax)...
    method not allowed 라고 뜸...
    
    일단 html부터 다시 손 보는거로

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import requests
+from pymongo import MongoClient
+
 
 app = Flask(__name__)
 
@@ -7,13 +10,25 @@ app = Flask(__name__)
 def home_jack_and_beantree():
     return render_template('index.html')
 
-@app.route('/books')
-def book_tree_current():
-    return render_template('booktree_page.html')
+# @app.route('/books')
+# def book_tree_current():
+#     return render_template('booktree_page.html')
 
-@app.route('/books/add')
+@app.route('/books', methods=['GET'])
 def book_tree_adding():
     return render_template('booktree_add.html')
+
+def book_added():
+    title = request.form['title'];
+    author = request.form['author'];
+    page = request.form['title'];
+    isbn = request.form['isbn'];
+    sentence = request.form['sentence'];
+    comment = request.form['comment'];
+    date = request.form['date'];
+    rate =request.form['rate'];
+
+    print(title, author, page, isbn, sentence, comment, date, rate);
 
 @app.route('/booktree')
 def book_tree_now():
