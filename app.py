4afa7d17aadaa151cefda3c5bbab2add4c56cd93
minjoo <kim9min3joo@gmail.com>
2021-01-12 17:26:09 +0900

    app.py 5
    isbn 입력하고 책 정보 가져오는거 해봤으나 안됨 ㅇㅅㅇㅇ

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 import requests
 from pymongo import MongoClient
+from bs4 import BeautifulSoup
 
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
@@ -42,6 +43,38 @@ def adding_book_tree():
 
     db.booktree.insert_one(doc)
     return jsonify({'result': 'success', 'msg': '잭의 책나무가 한뼘 자라났습니다!'})
+
+
+@app.route('/booksadd/search', methods=['POST'])
+def searching_book_info():
+    isbn = request.form['isbn']
+
+    url = 'https://www.aladin.co.kr/shop/wproduct.aspx?ISBN=' + isbn
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url, headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    image = soup.select_one('#CoverMainImage')
+    title = soup.select_one(
+        '#Ere_prod_allwrap > div.Ere_prod_topwrap > div.Ere_prod_titlewrap > div.left > div > ul > li:nth-child(1) > div > a.Ere_bo_title')
+    author = soup.select_one(
+        '#Ere_prod_allwrap > div.Ere_prod_topwrap > div.Ere_prod_titlewrap > div.left > div > ul > li.Ere_sub2_title > a:nth-child(1)')
+    page = soup.select_one(
+        '#Ere_prod_allwrap > div.Ere_prod_middlewrap > div:nth-child(1) > div.Ere_prod_mconts_R > div.conts_info_list1 > ul > li:nth-child(1)')
+
+
+    info = {
+        'image': image,
+        'title': title,
+        'author': author,
+        'page': page
+    }
+
+    db.bookinfo.insert_one(info)
+
+    return jsonify({'result': 'success', 'msg': '책 정보 가져오기 성공!'})
 
 
 @app.route('/booktree')
